4b314c3d1b1201a6d7c1c3e6ea5ff756d820e32a
Ramiro Matteoda <ramiro.matteoda@3sharecorp.com>
2018-07-17 18:29:49 -0300

    remove not used dialog improve todo list

--- ui.apps/src/main/content/jcr_root/apps/spa-vue/components/content/vue/todo-list/clientlib/js/todoitem.js
+++ ui.apps/src/main/content/jcr_root/apps/spa-vue/components/content/vue/todo-list/clientlib/js/todoitem.js
@@ -1,10 +1,31 @@
 
 const todoitem = {
     name: 'todoitem',
+    el: '#example-1',
     component: {
-       props: ['text', 'number'],
-       template: `<li :id="number">{{number}} : {{text}}</li>`
-    }
+        props: ['number'],
+        template: `<ul id="example-1">
+					  <li v-for="item in items">
+					    {{ item }}
+					  </li>
+					</ul>`,
+        data() {
+	      return {
+	        items: this.getTodoItems(),
+	      };
+		},
+	    methods: {
+		    getTodoItems() {
+		      const todoItemsRange = new Array(this.number);
+		      var taskId = 0;
+		      for (let i = 0; i < this.number; i += 1) {
+				taskId = i + 1;
+		        todoItemsRange[i] = 'Task ' + taskId;
+		      }
+		      return todoItemsRange;
+		    }
+		}
+	}  
 }
 
 export default todoitem
